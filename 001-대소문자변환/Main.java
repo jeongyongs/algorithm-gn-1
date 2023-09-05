bdf9b6ef04aab465cbd63a096f9a0d32165c7566
Jeongyong Lee <hua9378@gmail.com>
2023-09-05 10:32:37 +0900

    문제풀이

--- 001-대소문자변환/Main.java
+++ 001-대소문자변환/Main.java
@@ -1,15 +1,11 @@
 public class Main {
     private static double correctCount = 0;
-    private static final String[][] TESTCASES = new String[][] {
-            { "MANGO", "mango" },
-            { "peach", "PEACH" },
-            { "Apple", "aPPLE" },
-            { "baNANa", "BAnanA" },
-            { "Believe In YourSelF", "bELIEVE iN yOURsELf" },
-            { "THiS is how LIFE is!!", "thIs IS HOW life IS!!" },
-            { "Li^^ve  p$osi&TIVE*", "lI^^VE  P$OSI&tive*" },
-            { "%tiMe~~~~ iS>.< gggggolD ?:)", "%TImE~~~~ Is>.< GGGGGOLd ?:)" }
-    };
+    private static final String[][] TESTCASES =
+            new String[][] {{"MANGO", "mango"}, {"peach", "PEACH"}, {"Apple", "aPPLE"},
+                    {"baNANa", "BAnanA"}, {"Believe In YourSelF", "bELIEVE iN yOURsELf"},
+                    {"THiS is how LIFE is!!", "thIs IS HOW life IS!!"},
+                    {"Li^^ve  p$osi&TIVE*", "lI^^VE  P$OSI&tive*"},
+                    {"%tiMe~~~~ iS>.< gggggolD ?:)", "%TImE~~~~ Is>.< GGGGGOLd ?:)"}};
 
     public static void main(String[] args) {
         for (int i = 0; i < TESTCASES.length; i++)
