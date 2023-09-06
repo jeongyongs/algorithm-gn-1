542b382c9697ae0f334578f099daaa9b8942c9a2
Jeongyong Lee <hua9378@gmail.com>
2023-09-07 08:28:15 +0900

    문제풀이

--- 003_1-진법변환_2to10_소수/Main.java
+++ 003_1-진법변환_2to10_소수/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -5,11 +7,9 @@ public class Main {
                 "0.00000001", "0.000000001", "0.0000000001", "0.00000000001"};
         double[] outputs = {2.25, 3.125, 9.203125, 20.8125, 23.625, 0.00390625, 0.001953125,
                 0.0009765625, 0.00048828125};
-
         double score = test(inputs, outputs);
 
         System.out.printf(" %d 개의 문제 중, %1.0f 개를 맞추었습니다.", inputs.length, score);
-
     }
 
     public static double test(String[] inputs, double[] outputs) {
@@ -42,8 +42,39 @@ public class Main {
         return score;
     }
 
-    private static double solution(String string) {
+    private static double solution(String input) {
+        String[] splittedString = input.split("\\.");
 
-        return 0;
+        return integerPart(splittedString[0]) + decimalPart(splittedString[1]);
+    }
+
+    private static int integerPart(String input) {
+        int[] ints = splitInput(input);
+        int result = 0;
+
+        for (int i = 0; i < ints.length; i++) {
+            if (ints[i] == 0) {
+                continue;
+            }
+            result += Math.pow(2, ints.length - i - 1);
+        }
+        return result;
+    }
+
+    private static double decimalPart(String input) {
+        int[] ints = splitInput(input);
+        double result = 0.0;
+
+        for (int i = 0; i < ints.length; i++) {
+            if (ints[i] == 0) {
+                continue;
+            }
+            result += Math.pow(0.5, i + 1);
+        }
+        return result;
+    }
+
+    private static int[] splitInput(String input) {
+        return Arrays.stream(input.split("")).mapToInt(Integer::parseInt).toArray();
     }
 }
